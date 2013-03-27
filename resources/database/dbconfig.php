37e69bc9315bec5bea891a294052bfe3047fc1c6
Vrajesh Patel <vj11590@gmail.com>
2013-03-26 20:04:15 -0400

    first commit

---
+++ resources/database/dbconfig.php
@@ -0,0 +1,11 @@
+<?php
+    //database configuration file
+
+    define('db_host', "localhost");
+    define('db_name', "localhost");
+    define('db_pass', "password");
+    define('db_user', "localhost");
+    echo ("dbconfig.php loaded");
+
+
+?>

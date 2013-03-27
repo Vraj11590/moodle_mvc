0e885922158a9baedf848ae2bb48a779b3811d0e
Vrajesh Patel <vj11590@gmail.com>
2013-03-26 20:03:51 -0400

    first commit

---
+++ models/base/dbconnection.class.php
@@ -0,0 +1,29 @@
+<?php
+
+//returns a connection object that can be used anywhere.
+
+    class Database{
+        
+        private $connection = null; //this variable can be passed to anything that needs a database connection
+        
+        public function __construct()
+        {
+            if (!$this->connection)
+            {
+            // create db connection, using the constants from config/db.php
+                $this->connection = new mysqli(db_host, db_user, db_pass, db_name);                        
+            // if no connection errors: return true else false
+                return (!$this->connection->connect_errno ? true : false);
+            }
+        }
+        
+        public function getDatabaseConnection()
+        {
+            return $this->connection;
+        }
+        public function getErrorNo()
+        {
+            return $this->connection->connect_errno;
+        }
+    }
+?>
---
+++ models/login/login_validate.php
@@ -0,0 +1,23 @@
+<?php
+
+//checks db and returns boolean value depending on if ucid/pw is good or bad
+    class check{
+
+        public function __construct(Database $db)
+        {
+           $this->connection = $db->getDatabaseConnection();                   // get the database connection
+            
+            
+            
+            
+        }
+        
+        
+    }
+    
+    
+    
+
+
+
+?>

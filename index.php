63f5d4aab53bef65540ce56990cba82f49538074
Vrajesh Patel <vj11590@gmail.com>
2013-03-26 20:05:27 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+   require_once('resources/database/dbconfig.php');
+   include('models/base/dbconnection.class.php');
+   include('controllers/login/login_control.class.php');
+
+    $db = new Database();
+    $login = new Login($db);
+    if($login->isUserLoggedIn())
+    {
+        include('views/post_login/main.php');
+
+    }
+    else
+    {
+        include('views/pre_login/login.php');
+    }
+   
+   
+?>

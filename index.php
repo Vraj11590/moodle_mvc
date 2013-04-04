d1d45cead050315602b62397a77304341a35296b
Vraj11590 <vj11590@gmail.com>
2013-04-04 18:53:03 -0400

    second commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
    require_once('resources/database/dbconfig.php');
    include('models/base/dbconnection.class.php');
    include('controllers/login/login_control.class.php');
-
+   include('controllers/student/student_control.class.php');
     $db = new Database();
     $login = new Login($db);
     if($login->isUserLoggedIn())

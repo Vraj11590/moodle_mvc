396fcbe635382e276c804e94e7aedfeac905642b
Thienvuong H Huynh unknown student <thh4@afsconnect2.njit.edu>
2013-04-06 13:37:39 -0400

    this folder contents the query

---
+++ resources/database/queries/queries.sql
@@ -0,0 +1,17 @@
+--this query gets all the courses the student is register for, it includes
+-- previous semesters and current semester. return a table.
+
+SELECT 
+e.ucid as studentid,
+s.semesterid as sid,s.crn as crn, s.sectionid AS sectionid,
+c.courseid AS courseid,c.coursename AS coursename, 
+u.name AS teachername
+
+FROM  courses AS c, sections AS s, users AS u, enrolled AS e
+
+WHERE s.teacherid = u.ucid
+AND s.courseid = c.courseid
+AND s.crn = e.crn
+AND e.ucid =  'thh4'
+
+GROUP BY s.crn

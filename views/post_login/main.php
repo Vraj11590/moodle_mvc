403a9a570e53017292deafed011f7f7db757c10f
Vraj11590 <vj11590@gmail.com>
2013-04-06 18:28:17 -0400

    new design

--- views/post_login/main.php
+++ views/post_login/main.php
@@ -3,13 +3,32 @@
 <?php
 $studentModel = new studentModel($db,$_SESSION['UCID']);
 $student = new studentControl($studentModel);
+$semester = "Spring 2013";
 ?>
- 
+<script>
+    var test = <?php $semester; ?>;
+    
+</script>
 <link rel="stylesheet" type="text/css" href="views/post_login/css/structure.css">
 
  <div id = "main">
-     
-     <div id = "header">
+    <center><div id="logo"><img src="http://i1321.photobucket.com/albums/u544/vj11590/logo_zps0cbcb553.jpg" width:"960px"></div></center>
+    <div id = "header">
+         <div id="semestercourse">
+            <div id ="semesterpanel">
+               <center>
+               <table>
+                    <form action="" name="semester" id="semester_select">
+                         <label>Select Semester:</label><br>
+                         <select>
+                              <option id="test" value="3"> d </option>
+                         </select>
+                    </form>
+               </table>
+               </center>
+            </div>
+          </div>
+         
           <div id="userpanel">
                <div id = "user_top">
                          <label>Hey, <?php echo $_SESSION['NAME']; ?>  !</label>
@@ -26,25 +45,14 @@ $student = new studentControl($studentModel);
                </div>
 
           </div>
-          <div id="semesterpanel">
-               <center>
-               <table>
-                    <form action="" name="semester" id="semester_select">
-                         <label>Select Semester:</label><br>
-                         <select>
-                              <option value=""> <?php ?> </option>
-                         </select>
-                    </form>
-               </table>
-               </center>
-          </div>
      </div>
-     
-     
-     
-     
-     
-     
+    <div id = "content">
+        
+<!--        <div id = "leftpanel"></div>
+        <div id = "rightpanel"></div>-->
+    </div>
+
+
  </div>
  
  <?php include('views/base/footer.php'); ?>

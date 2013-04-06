4b5e87463f6ebfe63ab82ba1c907100bda909dfa
Thienvuong H Huynh unknown student <thh4@afsconnect2.njit.edu>
2013-04-06 13:26:41 -0400

    test change

--- resources/database/dbcreate.sql
+++ resources/database/dbcreate.sql
@@ -10,7 +10,7 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Database: `thh4`
+-- Database: `thh4`  random change randsafasdklajdljasfjslfdjfljfajf
 --
 
 -- --------------------------------------------------------

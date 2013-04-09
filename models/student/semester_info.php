5f8e396b82e61e39008260fe70a33037570fdc2d
thh4 <thh4@njit.edu>
2013-04-09 07:00:53 -0300

    semester_info.php
    through ucid and semesterid, return the value of coursed, coursename, sectionid, crn, and teachername

---
+++ models/student/semester_info.php
@@ -0,0 +1,83 @@
+<?php
+
+
+
+class studentModel{
+       
+    private $ucid = "";
+  private $semesterid="";
+    private $connection = "";
+    public $studentData = "";
+    
+	public $crn = array();
+	public $teachername=array();
+    public $courseid = array();
+    public $courseNames = array();
+	public $sectionid=array();
+	
+    
+    
+    public function __construct(Database $db,$session_ucid)
+    {
+       //set globals
+        $this->connection = $db->getDatabaseConnection();
+        $this->ucid = $session_ucid;
+       
+       
+
+    }
+    public function setStudentData($data)
+    {
+       $this->studentData = $data;
+    }
+    public function getSemesterData($ucid,$semesterid)()
+    {
+        $count= 0;
+       if($this->connection)
+       {
+              $result = mysqli_query($this->connection,"SELECT e.ucid as studentid,
+															   c.courseid AS courseid, c.coursename AS coursename,
+															   s.crn as crn, s.sectionid as sectionid,
+															   u.name as teachername
+															   FROM sections as s, enrolled as e, courses as c, users as u
+															   WHERE e.crn = s.crn
+															     AND s.courseid = c.courseid
+															     AND s.teacherid = u.ucid
+                                                                 AND e.ucid='".$this->ucid."' 
+                                                                 AND s.semesterid='".$this->semesterid."'");
+              while($row = mysqli_fetch_array($result))
+              {
+                $crn[] = $row['crn'];
+				$courseid[] = $row['courseid'];
+				$coursename[] = $row['coursename'];
+				$sectionid[] = $row['sectionid'];
+				$teachername[] = $row['teacher'];
+              }
+            //echo $courseNames[];
+            echo json_encode($coursename);
+			echo json_encode($courseid);
+			echo json_encode($sectionid);
+			echo json_encode($teachername);
+			echo json_encode($crn);
+            
+            //return json_encode($courseName);
+              
+       }else{
+        echo("connection error");
+       }
+    }
+    
+
+    
+    
+    
+    
+    public function getStudentData()
+    {
+        try{return $this->studentData;}
+        catch(Exception $e){echo ($e->getMessage());}
+    }
+    
+}
+?>
+
